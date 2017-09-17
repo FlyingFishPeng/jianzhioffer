65898192213cbcfee0fba1f705bba8b9959af743
FlyingFishPeng <844495506@qq.com>
2017-09-17 14:24:29 +0800

    Update Mirror.cpp

--- Mirror.cpp
+++ Mirror.cpp
@@ -1,9 +1,9 @@
 /*
-  ��Ŀ���������ľ��񡣲��������Ķ�����������任ΪԴ�������ľ���
+  题目：二叉树的镜像。操作给定的二叉树，将其变换为源二叉树的镜像。
 */
 
 /*
-  ˼·�����ϵ������ν������ҽ�㡣
+  思路：从上到下依次交换左右结点。
 */
 
 /*
@@ -15,6 +15,7 @@ struct TreeNode {
 			val(x), left(NULL), right(NULL) {
 	}
 };*/
+//递归
 class Solution {
 public:
     void Mirror(TreeNode *pRoot) {
@@ -24,14 +25,38 @@ public:
         if(pRoot->left == nullptr && pRoot->right == nullptr)
             return;
 
-        TreeNode *ptmp = pRoot->left;   //�������
+        TreeNode *ptmp = pRoot->left;   //交换结点
         pRoot->left = pRoot->right;
         pRoot->right = ptmp;
 
-        if(pRoot->left != nullptr)     //�����������Ľ���
+        if(pRoot->left != nullptr)     //处理左子树的交换
             Mirror(pRoot->left);
 
-        if(pRoot->right != nullptr)    //�����������Ľ���
+        if(pRoot->right != nullptr)    //处理右子树的交换
             Mirror(pRoot->right);
     }
+};
+
+//非递归
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+        if(pRoot==NULL)
+            return;
+        stack<TreeNode*> stackNode;
+        stackNode.push(pRoot);
+        while(stackNode.size()){
+            TreeNode* tree=stackNode.top();
+            stackNode.pop();
+            if(tree->left!=NULL || tree->right!=NULL){
+                TreeNode *ptemp=tree->left;
+                tree->left=tree->right;
+                tree->right=ptemp;
+            }
+            if(tree->left)
+                stackNode.push(tree->left);
+            if(tree->right)
+                stackNode.push(tree->right);
+        }
+    }
 };
